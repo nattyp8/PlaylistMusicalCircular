d2c6c6cd95b748df357fb04eec62e2fddc0e5f01
Natalia Vallejos <nataliaargana365@gmail.com>
2026-06-20 01:46:49 -0300

    se agrega metodos en playlist de estructuras

--- PlaylistMusicalCircular/Estructuras/PlaylistCircular.cs
+++ PlaylistMusicalCircular/Estructuras/PlaylistCircular.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using PlaylistMusicalCircular.Modelos;
+using System.Collections.Generic;
 
 namespace PlaylistMusicalCircular.Estructuras
 {
@@ -21,5 +22,120 @@ namespace PlaylistMusicalCircular.Estructuras
 
             cantidad = 0; // inicialmente hay 0 canciones 
         }
+    
+
+
+    // METODOS
+
+    //METODO PARA AGREGAR CANCION 
+         public void AgregarCancion(Cancion nuevaCancion)
+        {
+            NodoCancion nuevoNodo = new NodoCancion(nuevaCancion);
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+
+                actual = nuevoNodo;
+
+                nuevoNodo.Siguiente = nuevoNodo;
+
+                nuevoNodo.Anterior = nuevoNodo;
+            }
+            else
+            {
+                NodoCancion ultimo = cabeza.Anterior;
+                nuevoNodo.Siguiente = cabeza;
+                nuevoNodo.Anterior = ultimo;
+                ultimo.Siguiente = nuevoNodo;
+                cabeza.Anterior = nuevoNodo;
+            }
+
+            cantidad++;
+        }
+
+        //METODO OBTENER CANCION ACTUAL
+        public Cancion ObtenerCancionActual()
+        {
+            if (actual==null)
+            {
+                return null;
+            }
+
+            return actual.Datos;
+        }
+
+        //METODO PARA LA SIGUIENTE CANCION
+
+        public void Siguiente()
+        {
+            if(actual != null)
+            {
+                actual = actual.Siguiente;
+            }
+        }
+
+        //METODO PARA LA CANCION ACTUAL
+        public void Anterior()
+        {
+            if (actual != null)
+            {
+                actual = actual.Anterior;
+            }
+        }
+
+        //METODO PARA CANTIDAD DE CANCIONES
+        public int CantidadCanciones()
+        {
+            return cantidad;
+        }
+
+        //METODO PARA BUSCAR CANCIONES
+        public Cancion BuscarCancion(string titulo)
+        {
+
+            if (cabeza == null)
+            {
+                return null;
+            }
+
+            NodoCancion aux = cabeza;
+
+            do
+            {
+                if (aux.Datos.Titulo.Equals(titulo, StringComparison.OrdinalIgnoreCase))
+                {
+                    return aux.Datos;
+                }
+
+                aux = aux.Siguiente;
+            } while (aux != cabeza);
+
+            return null;
+
+        }
+
+        // METODO PARA TRAER TODAS LAS CANCIONES
+
+        public List<Cancion> ObtenerTodas()
+        {
+            List<Cancion> canciones = new List<Cancion>();
+
+            if (cabeza == null)
+            {
+                return canciones;
+            }
+
+            NodoCancion aux = cabeza;
+
+            do
+            {
+                canciones.Add(aux.Datos);
+                aux = aux.Siguiente;
+            } while (aux != cabeza);
+            return canciones;
+            
+        }
+
     }
 }
+
